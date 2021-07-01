6b95d8aeb6329d4cb3f2fb9fc9223a41ece8bf87
Mark <mark.mazur007@gmail.com>
2021-07-01 20:08:43 +0300

    added create and updata actions

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,17 +1,52 @@
 class ItemsController < ApplicationController
-  layout false
+
   skip_before_action :verify_authenticity_token
 
   def index
     @items = Item.all
-
-    render body: @items.map { |i| "#{i.name}:#{i.price}" }.join(', ')
   end
 
   def create
     item = Item.create(items_params)
     if item.persisted?
-      render json: item.name, status: :created
+      redirect_to item_path(item)
+    else
+      render json: item.errors, status: :unprocessable_entity
+    end
+  end
+
+  def new
+
+  end
+
+  def show
+    unless (@item = Item.where(id: params[:id]).first)
+      render body: 'Page not found', status: 404
+    end
+  end
+
+  def edit
+    unless (@item = Item.where(id: params[:id]).first)
+      render body: 'Page not found', status: 404
+    end
+  end
+
+  def update
+    item = Item.where(id: params[:id]).first
+
+    if item.update(items_params)
+      redirect_to item_path
+    else
+      render json: item.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    raise
+    item = Item.where(id: params[:id]).first.destroy
+
+    if item.destroyed?
+      redirect_to items_path
     else
       render json: item.errors, status: :unprocessable_entity
     end
@@ -20,6 +55,6 @@ class ItemsController < ApplicationController
   private
 
   def items_params
-    params.permit(:name, :price)
+    params.permit(:name, :price, :description)
   end
 end
